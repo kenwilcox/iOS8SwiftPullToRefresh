a80f76e277a1f5b23f266956c8d78909d3c1e047
kenwilcox <wilcoxk@gmail.com>
2015-01-04 21:23:43 -0700

    pull to "refresh" changes the sort order

--- iOS8SwiftPullToRefresh/TableViewController.swift
+++ iOS8SwiftPullToRefresh/TableViewController.swift
@@ -11,15 +11,25 @@ import UIKit
 class TableViewController: UITableViewController {
   
   var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var isAscending = true
+  
+  func sortArray() {
+    var sortedAlphabet = alphabet.reverse()
+    
+    for (index, element) in enumerate(sortedAlphabet) {
+      alphabet[index] = element
+    }
+    
+    tableView.reloadData()
+    refreshControl?.endRefreshing()
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    var refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: Selector("sortArray"), forControlEvents: UIControlEvents.ValueChanged)
+    self.refreshControl = refreshControl
   }
   
   override func didReceiveMemoryWarning() {
