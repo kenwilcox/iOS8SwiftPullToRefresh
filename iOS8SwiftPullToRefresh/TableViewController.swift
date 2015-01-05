f9fea1caeae2e3809db6ba86b39e073bdc910c31
kenwilcox <wilcoxk@gmail.com>
2015-01-04 21:20:42 -0700

    Load Table with Data

--- iOS8SwiftPullToRefresh/TableViewController.swift
+++ iOS8SwiftPullToRefresh/TableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TableViewController: UITableViewController {
   
+  var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -30,24 +32,26 @@ class TableViewController: UITableViewController {
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     // #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0
+    return 1
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     // #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0
+    return alphabet.count
   }
   
-  /*
+  //*
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-  let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-  
-  // Configure the cell...
-  
-  return cell
+    //let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+    
+    // Configure the cell...
+    cell.textLabel?.text = alphabet[indexPath.row]
+    
+    return cell
   }
-  */
+  //*/
   
   /*
   // Override to support conditional editing of the table view.
